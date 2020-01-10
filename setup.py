12a2f6dee1d0bdb3d68e01401559812fd6da7dbc
Stephane Bottine <stephane.bottine@gmail.com>
2020-01-10 20:38:01 +0000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='yourmum',
+      version='0.1',
+      description='Probably the funniest your mum jokes! Customize jokes to use \'mum\', \'mom\', \'momma\' or any other qualifier.',
+      url='')
